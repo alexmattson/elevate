de2fdfbaab934ac4cc08eb0abdf9495b4173f83f
Paul Oliva <paulmoliva@gmail.come>
2017-02-11 12:47:36 -0800

    create indexhtml

--- backend/app/controllers/pages_controller.rb
+++ backend/app/controllers/pages_controller.rb
@@ -1,2 +1,5 @@
 class PagesController < ApplicationController
+    def index
+        render file: Rails.root.join('public', 'index.html')
+    end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
     get '/', to: 'pages#index'
     post '/polls', to: 'polls#new'
+    get '*other', to: 'pages#index'
 end
