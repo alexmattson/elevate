9df7d97d86b406e0514e6dc48df9331df73c60c4
Paul Oliva <paulmoliva@gmail.come>
2017-02-11 12:06:38 -0800

    add pages controller

--- backend/app/controllers/polls_controller.rb
+++ backend/app/controllers/polls_controller.rb
@@ -1,2 +1,11 @@
 class PollsController < ApplicationController
+
+    def new
+        name = params[:name]
+        description = params[:description]
+        type = params[:type]
+        @poll = Poll.new(name, description, type)
+        if @poll.save
+            render json:
+    end
 end
