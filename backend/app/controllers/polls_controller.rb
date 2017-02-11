5b7bfe408cf8adf488fe2a795aad1a98e0937dbb
Paul Oliva <paulmoliva@gmail.come>
2017-02-11 13:41:41 -0800

    save

--- backend/app/controllers/polls_controller.rb
+++ backend/app/controllers/polls_controller.rb
@@ -1,14 +1,19 @@
 class PollsController < ApplicationController
     def new
-        @poll = Poll.new
-        @poll.name = params[:name]
-        @poll.description = params[:description]
-        @poll.type = params[:type]
-        @poll.set_token
+        body = JSON.parse(request.body.read)
+        byebug
+        @poll = Poll.new(body)
         if @poll.save
             render json: @poll
         else
             render json: 'error saving poll', status: 410
         end
+    end
+
+    def show
+        byebug
+        token_id = params[:token_id]
+        @poll = Poll.find_by(token: token_id)
+        render json: @poll
     end
 end
